4b9cd973720fc35aeec8e989d9f127f52949bf55
silver takana <titmitna@gmail.com>
2021-12-02 07:41:58 +1300

    better collision detection

--- engine.js
+++ engine.js
@@ -1,83 +1,84 @@
 class Game {
-    constructor() {
-        this.changes = []
-        this.sx = 0
-    }
-    getTile(x1, y1) {
-        const x = floor(x1),y=floor(y1)
-        // const x = x1,y=y1
-        // for (let i in this.changes) {
-        //     let t = this.changes[i];
-        //     if (t.x === x && t.y === y) {
-        //         // print("hi")
-        //         return t.id;
-        //     }
-        // }
-        let ischanged = false   
-        let change
-        this.changes.forEach((t)=>{
-            if (t.x === x && t.y === y) {
-                ischanged = true
-                change = t.id;
-            }
-        })
-        if(ischanged) return change
-        push()
-        let he = (noise(x / 40)) * 50
-        let coal = noise(x / 3, y / 3);
-        noiseDetail(2,2)
-        let cave = noise(x / 20 + 200, y / 20 + 200)
-        
-        if (cave > 0.95) {
-            return 0
-        } else {
-            if (y > he) {
-                return 0
-            } else if (y === floor(he)) {
-                return 1
-            } else if (y > he + coal * 5) {
-                return 2
-            } else {
-                if (coal > 1) {
-                    return 4
-                } else {
-                    return 3
-                }
+	constructor() {
+		this.changes = [];
+		this.sx = 0;
+	}
+	getTile(x1, y1) {
+		const x = floor(x1),
+			y = floor(y1);
+		// const x = x1,y=y1
+		// for (let i in this.changes) {
+		//     let t = this.changes[i];
+		//     if (t.x === x && t.y === y) {
+		//         // print("hi")
+		//         return t.id;
+		//     }
+		// }
+		let ischanged = false;
+		let change;
+		this.changes.forEach((t) => {
+			if (t.x === x && t.y === y) {
+				ischanged = true;
+				change = t.id;
+			}
+		});
+		if (ischanged) return change;
+		let he = noise(x / 40) * 50;
+		let coal = noise(x / 3, y / 3);
+		push();
+		noiseDetail(2, 2);
+		pop();
+		let cave = noise(x / 20 + 200, y / 20 + 200);
 
-            }
-        }
-        pop()
-
-    }
-    setTile(x1, y1, id) {
-        const x = round(x1),y=round(y1)
-        for (let i in this.changes) {
-            let t = this.changes[i];
-            if (t.x === x && t.y === y) {
-                this.changes[i] = { x: x, y: y, id: id }
-                return;
-            }
-        }
-        if(this.getTile(x,y)!== id){
-            this.changes.push({ x: x, y: y, id: id });
-        }
-        
-    }
-    render(){
-        let cX = camX, cY = camY;
-        for (let i = 0; i < ceil(width / bw) + 1; i++) {
-            for (let j = 0; j < ceil(height / bw) + 1; j++) {
-                let tilex = -(i - floor(cX)), tileY = -(j - floor(cY));
-                let bl = game.getTile(tilex, tileY);
-
-                if (bl !== 0) {
-                    image(
-                        tex[bl],
-                        (cX - tilex - 1) * bw,
-                        (cY - tileY - 1) * bw,
-                        bw, bw)
-                }
-            }
-        }
-    }
+		if (cave > 0.95) {
+			return 0;
+		} else {
+			if (y > he) {
+				return 0;
+			} else if (y === floor(he)) {
+				return 1;
+			} else if (y > he + coal * 5) {
+				return 2;
+			} else {
+				if (coal > 1) {
+					return 4;
+				} else {
+					return 3;
+				}
+			}
+		}
+	}
+	setTile(x1, y1, id) {
+		const x = round(x1),
+			y = round(y1);
+		for (let i in this.changes) {
+			let t = this.changes[i];
+			if (t.x === x && t.y === y) {
+				this.changes[i] = { x: x, y: y, id: id };
+				return;
+			}
+		}
+		if (this.getTile(x, y) !== id) {
+			this.changes.push({ x: x, y: y, id: id });
+		}
+	}
+	render() {
+		let bl, tilex, tileY;
+		for (let i = 0; i < ceil(width / bw) + 1; i++) {
+			for (let j = 0; j < ceil(height / bw) + 1; j++) {
+				tilex = -(i - floor(camX));
+				tileY = -(j - floor(camY));
+				bl = this.getTile(tilex, tileY);
+				if (bl !== 0) {
+					image(
+						tex[bl],
+						(camX - tilex - 1) * bw,
+						(camY - tileY - 1) * bw,
+						bw,
+						bw
+					);
+				}
+			}
+		}
+	}
 }
--- script.js
+++ script.js
@@ -1,66 +1,72 @@
-var tex = [], game, camX = 0, camY = 0
-var loaded = false
-var player
-const bw = 50
-function preload() {
-
-}
+var tex = [],
+	game,
+	camX = 0,
+	camY = 0;
+var loaded = false;
+var player;
+const bw = 50;
+function preload() {}
 function setup() {
-    loadFiles();
-    noiseSeed(666)
-    createCanvas(windowWidth, windowHeight);
-    game = new Game();
-    player = new Player(-3,40,40,0.85);
-    game.setTile(0,0,1)
+	loadFiles();
+	noiseSeed(666);
+	createCanvas(windowWidth, windowHeight);
+	game = new Game();
+	player = new Player(-3, 40, 40, 0.85);
+	game.setTile(0, 0, 1);
 }
 
 function draw() {
+	push();
+	if (!loaded) {
+	} else {
+		if (mouseIsPressed) {
+			if (keyIsDown(69)) {
+				game.setTile(camX - mouseX / bw - 0.5, camY - mouseY / bw - 0.5, 5);
+			} else if (keyIsDown(81)) {
+				game.setTile(camX - mouseX / bw - 0.5, camY - mouseY / bw - 0.5, 0);
+			}
+		}
+		background(255);
+		game.render();
+		if (keyIsDown(32)) {
+			player.control(10000000000000);
+		} else {
+			player.control(0.03);
+		}
 
-    if (!loaded) {
-
-
-    } else {
-        if(mouseIsPressed){
-             if (keyIsDown(69)) {
-            game.setTile(camX - mouseX / bw - 0.5, camY - mouseY / bw - 0.5, 5)
-        }   else if(keyIsDown(81)){
-            game.setTile(camX - mouseX / bw - 0.5, camY - mouseY / bw - 0.5, 0)
-            }
-        }
-        background(255);
-        game.render();
-        if(keyIsDown(32)){
-            player.control(10000000000000) 
-        }else{
-            player.control(0.03)  
-        }
-        
-        camX = player.pos.x+ width/(2*bw)
-        camY = player.pos.y + height/(2*bw)
-        player.show();
-    }
-    push()
-    translate(0,0)
-    text("x: "+camX+"y: "+camY, 50, 50)
-    pop()
+		camX = player.pos.x + width / (2 * bw);
+		camY = player.pos.y + height / (2 * bw);
+		player.show();
+	}
+	pop();
+	text(
+		"(x: " +
+			round(camX, 3) +
+			" y: " +
+			round(camY, 3) +
+			") fps: " +
+			round(frameRate(), 3),
+		50,
+		50
+	);
 }
 function loadFiles() {
-    let maxfile = 4;
-    let fileLoaded = 1
-    let limg = (name) => {
-        fileLoaded++
-        if (fileLoaded >= maxfile) {
-            loaded = true;
-        }
-        let file = loadImage(name)
+	let maxfile = 4;
+	let fileLoaded = 1;
+	let limg = (name) => {
+		fileLoaded++;
+		if (fileLoaded >= maxfile) {
+			loaded = true;
+		}
+		let file = loadImage(name);
 
-        return file
-    }
-    tex[1] = limg("Texture/Grass.jpeg");
-    tex[2] = limg("Texture/Dirt.jpeg");
-    tex[3] = limg("Texture/Stone.jpeg");
-    tex[4] = limg("Texture/Coal.jpeg");
-    tex[5] = limg("Texture/Cobble_stone.png");
+		return file;
+	};
+	tex[1] = limg("Texture/Grass.jpeg");
+	tex[2] = limg("Texture/Dirt.jpeg");
+	tex[3] = limg("Texture/Stone.jpeg");
+	tex[4] = limg("Texture/Coal.jpeg");
+	tex[5] = limg("Texture/Cobble_stone.png");
 }
 /*
 function control(speed) {
@@ -82,19 +88,19 @@ function control(speed) {
 }
 */
 function control(speed) {
-    if (keyIsDown(65)) {
-        camX += speed;
-    }
+	if (keyIsDown(65)) {
+		camX += speed;
+	}
 
-    if (keyIsDown(68)) {
-        camX -= speed;
-    }
+	if (keyIsDown(68)) {
+		camX -= speed;
+	}
 
-    if (keyIsDown(87)) {
-        camY += speed;
-    }
+	if (keyIsDown(87)) {
+		camY += speed;
+	}
 
-    if (keyIsDown(83)) {
-        camY -= speed;
-    }
+	if (keyIsDown(83)) {
+		camY -= speed;
+	}
 }
