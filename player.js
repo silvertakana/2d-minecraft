d18072fdcb8dc85244b0c8e81eef29e2e9147d4a
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-12-01 02:46:44 +0000

    Initial commit

---
+++ player.js
@@ -0,0 +1,67 @@
+class Player{
+    constructor(x,y,w,friction){
+        this.pos = createVector(x,y)
+        this.width = w
+        this.friction = friction;
+        this.vel = createVector(0,0);
+    }
+	isColliding(){
+		return game.getTile(this.pos.x,this.pos.y-this.width/bw) !== 0;
+	}
+    control(speed){
+		
+        if (keyIsDown(65)) {
+            this.vel.x += speed;
+        }
+    
+        if (keyIsDown(68)) {
+            this.vel.x -= speed;
+        }
+    
+        if (keyIsDown(87)) {
+            this.vel.y += speed;
+        }
+    
+        if (keyIsDown(83)) {
+            this.vel.y -= speed;
+        }
+        
+        this.vel.x *= this.friction
+        let touchy = this.isColliding();
+        if(touchy){
+            this.vel.y -= 0.005
+        }else{
+			this.vel.y = 0;
+			let moveAmount = 0.001;
+			let changeMoveAmount = 0.001;
+			while(touchy !== 0){
+				
+				this.pos.y -= moveAmount;
+				if(!this.isColliding()) break;
+				this.pos.y += moveAmount;
+
+				this.pos.y += moveAmount;
+				if(!this.isColliding()) break;
+				this.pos.y -= moveAmount;
+				
+				moveAmount+=changeMoveAmount;
+				
+			}
+            
+        }
+        
+        this.pos.add(this.vel)
+    }
+    show(){
+        let pos = this.pos
+        push()
+        fill(250)
+        
+        rectMode(CENTER)
+        // translate(width/2,height/2)
+        translate((camX - pos.x)*bw,(camY - pos.y)*bw);
+        rect(0,0,this.width,this.width*2)
+        pop()
+    }
+    
+}
---
+++ script.js
@@ -0,0 +1,100 @@
+var tex = [], game, camX = 0, camY = 0
+var loaded = false
+var player
+const bw = 50
+function preload() {
+
+}
+function setup() {
+    loadFiles();
+    noiseSeed(666)
+    createCanvas(windowWidth, windowHeight);
+    game = new Game();
+    player = new Player(-3,40,40,0.85);
+    game.setTile(0,0,1)
+}
+
+function draw() {
+
+    if (!loaded) {
+
+
+    } else {
+        if(mouseIsPressed){
+             if (keyIsDown(69)) {
+            game.setTile(camX - mouseX / bw - 0.5, camY - mouseY / bw - 0.5, 5)
+        }   else if(keyIsDown(81)){
+            game.setTile(camX - mouseX / bw - 0.5, camY - mouseY / bw - 0.5, 0)
+            }
+        }
+        background(255);
+        game.render();
+        if(keyIsDown(32)){
+            player.control(10000000000000) 
+        }else{
+            player.control(0.03)  
+        }
+        
+        camX = player.pos.x+ width/(2*bw)
+        camY = player.pos.y + height/(2*bw)
+        player.show();
+    }
+    push()
+    translate(0,0)
+    text("x: "+camX+"y: "+camY, 50, 50)
+    pop()
+}
+function loadFiles() {
+    let maxfile = 4;
+    let fileLoaded = 1
+    let limg = (name) => {
+        fileLoaded++
+        if (fileLoaded >= maxfile) {
+            loaded = true;
+        }
+        let file = loadImage(name)
+
+        return file
+    }
+    tex[1] = limg("Texture/Grass.jpeg");
+    tex[2] = limg("Texture/Dirt.jpeg");
+    tex[3] = limg("Texture/Stone.jpeg");
+    tex[4] = limg("Texture/Coal.jpeg");
+    tex[5] = limg("Texture/Cobble_stone.png");
+}
+/*
+function control(speed) {
+  if (keyIsDown(LEFT_ARROW)) {
+    camX += speed;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    camX -= speed;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    camY += speed;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    camY -= speed;
+  }
+}
+*/
+function control(speed) {
+    if (keyIsDown(65)) {
+        camX += speed;
+    }
+
+    if (keyIsDown(68)) {
+        camX -= speed;
+    }
+
+    if (keyIsDown(87)) {
+        camY += speed;
+    }
+
+    if (keyIsDown(83)) {
+        camY -= speed;
+    }
+}
